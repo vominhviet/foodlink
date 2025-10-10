cb9fa85d9389e4f46cf9e6f511c51ba16dc281d9
vominhviet <vominhviet2002@gmail.com>
2025-10-10 09:39:57 +0700

    add import in dashobord

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -224,6 +224,15 @@ export default function Dashboard() {
               </svg>
               Quản Lý Chi Phí
             </Link>
+             <Link
+              to="/import"
+              className="flex items-center gap-3 px-4 py-3 rounded-lg text-gray-700 hover:bg-blue-50 hover:text-blue-700 transition-all duration-200"
+            >
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1" />
+              </svg>
+              Quản lí nhập hàng
+            </Link>
           </nav>
         </div>
         
